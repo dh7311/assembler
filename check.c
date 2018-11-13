f3bf433d2017154f44972417d97bde3db5d4f9f3
dh7311 <dh7311@naver.com>
2018-11-13 21:15:02 +0900

    check make

--- check.c
+++ check.c
@@ -2,20 +2,23 @@
 
 int is_valid(char *op, char *args)
 {
- 	char a = {0};
-	char b = {0};
+ 	char *ptr1, *ptr2 = NULL;
+	if(strcmp(op,"mov") != 0 )
+	 	return 0;
+	char temp[256] = {0};
+	strcpy(temp,args);
+	ptr1 = strtok(temp,",");
+	ptr2 = strtok(NULL,"\n");
 
+	if(ptr1[0] == '(' || ptr1[0] == '-' || ptr1[0] == '0')
+	{
+	 	if(ptr2[0] =='(' || ptr2[0] =='-' || ptr2[0] =='0')
+		 	return 0;
+	}
+	else if(ptr1[0] == '$' && ptr2[0] == '$')
+	 	return 0;
+	else if(ptr2[0] == '$')
+	 	return 0;
 
-
-
-	//printf("if valid, return 1\n");
-	//printf("otherwise, return 0\n");
-
-	/********************************/
-    /*  							*/
-	/*         syntax check         */
-    /*  							*/
-	/********************************/
-	
 	return 1;
 }
